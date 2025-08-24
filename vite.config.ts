e46372904d1c301b86a5435ed449df947be31723
Raahim Ghori <raahimghori@gmail.com>
2025-08-24 02:13:03 -0400

    fixing some stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  base: '/British-Premier-Clinic/',
+  ],
+  base: "/British-Premier-Clinic/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
